d5f4680e44b233e00f01bc3206a209ce58c7b366
lokibg2 <lokibg2@gmail.com>
2015-12-27 01:53:22 +0530

    Self Transpose

---
+++ selfTranspose.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	int **mat, r, c, i, m, n, j, k;
+	r = 7, c = 7; // row & col values
+	
+	m = r > c ? r : c; //max of r & c
+	n = r < c ? r : c; //min of r&c
+
+	mat = (int **) malloc(sizeof(int*) * m); //allocate rows
+
+	for (i = 0; i < m; i++){
+	    mat[i] = (int *)malloc(m * sizeof(int)); //allocate columns
+	}
+
+	k = 1;
+	
+	for(i = 0; i < r ; ++i){
+		for (int j = 0; j < c; ++j){
+			//fill the matrix with values
+			mat[i][j] = k++;
+		}
+	}
+
+	for(i = 0; i < n ; ++i){
+		for (int j = i; j < m; ++j){
+			//swap
+			mat[i][j] += mat[j][i] - (mat[j][i] = mat[i][j]);
+		}
+	}
+
+	for(i = 0; i < c ; ++i){
+		for (int j = 0; j < r; ++j){
+			printf("%d\t", mat[i][j] );
+		}
+		printf("\n");
+	}	
+}
