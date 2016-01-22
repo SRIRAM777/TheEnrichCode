0a81dccbbf1b064c2e2c9560b5788d8400097152
K Lokeshwaran <lokibg2@gmail.com>
2016-01-22 15:53:10 +0530

    Update rebmuN.c

--- rebmuN.c
+++ rebmuN.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-int main()
+#include<stdio.h>
+int main(int argc, char const *argv[])
 {
 	long int n,r=0,s;
-  	scanf("%ld",&n);
-  	while(n!=0)
-  	{
-     	s=n%10;
-     	r=r*10+s;
-     	n/=10;
-  	}
-  	printf("%ld",r);
-  	return 0;
+	scanf("%ld",&n);
+	while(n!=0)
+	{
+		s=n%10;
+		r=r*10+s;
+		n/=10;
+	}
+	printf("%ld",r);
+	return 0;
 }
