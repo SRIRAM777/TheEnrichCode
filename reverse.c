6f2e9658918362b135be1c4a2a13fb08c07a9bc4
K Lokeshwaran <lokibg2@gmail.com>
2016-01-22 15:36:42 +0530

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,14 +1,1 @@
-#include <stdio.h>
-int main()
-{
-	long int n,r=0,s;
-  	scanf("%ld",&n);
-  	while(n!=0)
-  	{
-     	s=n%10;
-     	r=r*10+s;
-     	n/=10;
-  	}
-  	printf("%ld",r);
-  	return 0;
-}
+
